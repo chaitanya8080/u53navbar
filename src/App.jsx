c2de3ee52acad5d6d7b1ff4cc2dbe59ec5e2eaf3
chaitanya8080 <girase.chaitanya@gmail.com>
2022-04-10 22:50:05 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+     <div id="navbar"> 
+         <div><h2>LOGOBAkERY</h2></div>
+         <div id='name'>
+           <div><p>Search</p></div>
+           <div><p>Project</p></div>
+           <div><p>About</p></div> 
+         </div>
+         <div><button id='button1'>Contact</button></div>
+     </div>
+  );
+}
+
+export default App;
